9833b9955002fa8668de1b35b30a4ecbc4eae0ae
wei.li <wei.li@mtime.com>
2015-07-23 14:25:24 +0800

    小修改

--- src/main/java/org/jiagoushi/spring_mvc_basic/controller/HelloWorldController.java
+++ src/main/java/org/jiagoushi/spring_mvc_basic/controller/HelloWorldController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";//返回helloworld.jsp的文件名
+	}
+	
 	@RequestMapping("/hello")
 	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
 		model.addAttribute("name", name);
